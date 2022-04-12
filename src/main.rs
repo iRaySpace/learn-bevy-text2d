e69635337dad9678ee7c8141e2f1bd43c01787cd
irayspace <irayspacii@gmail.com>
2022-04-12 15:40:01 +0800

    feat: spawn keyboard

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use rand::prelude::*;
 
 #[derive(Component, Debug)]
 struct TextMoving {
@@ -6,34 +7,55 @@ struct TextMoving {
     duration: Timer,
 }
 
-fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let font = asset_server.load("FiraSans-Bold.ttf");
-    let text_style = TextStyle {
-        font,
-        font_size: 36.0,
-        color: Color::WHITE,
-    };
-    let text_alignment = TextAlignment {
-        vertical: VerticalAlign::Center,
-        horizontal: HorizontalAlign::Center,
-    };
+fn startup(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    commands
-        .spawn_bundle(Text2dBundle {
-            text: Text::with_section("Mission completed", text_style.clone(), text_alignment),
-            transform: Transform {
-                translation: Vec3::new(0.0, 0.0, 0.0),
-                ..Default::default()
-            },
-            ..Default::default()
-        })
-        .insert(TextMoving {
-            speed: 100.0,
-            duration: Timer::from_seconds(1.0, false),
-        });
 }
 
-fn text_system(mut commands: Commands, mut query: Query<(&mut TextMoving, &mut Transform, Entity)>, time: Res<Time>) {
+fn keyboard_spawn_text(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+) {
+    if keyboard_input.just_pressed(KeyCode::Space) {
+        let font = asset_server.load("FiraSans-Bold.ttf");
+        let text_style = TextStyle {
+            font,
+            font_size: 36.0,
+            color: Color::YELLOW,
+        };
+        let text_alignment = TextAlignment {
+            vertical: VerticalAlign::Center,
+            horizontal: HorizontalAlign::Center,
+        };
+        let mut rng = rand::thread_rng();
+        let x: i8 = rng.gen();
+        let y: i8 = rng.gen();
+        let damage: u8 = rng.gen();
+        commands
+            .spawn_bundle(Text2dBundle {
+                text: Text::with_section(
+                    format!("{:?}", damage),
+                    text_style.clone(),
+                    text_alignment,
+                ),
+                transform: Transform {
+                    translation: Vec3::new(x.into(), y.into(), 0.0),
+                    ..Default::default()
+                },
+                ..Default::default()
+            })
+            .insert(TextMoving {
+                speed: 100.0,
+                duration: Timer::from_seconds(1.0, false),
+            });
+    }
+}
+
+fn text_system(
+    mut commands: Commands,
+    mut query: Query<(&mut TextMoving, &mut Transform, Entity)>,
+    time: Res<Time>,
+) {
     let mut entities_to_dispose = vec![];
     for (mut text_moving, mut transform, entity) in query.iter_mut() {
         transform.translation.y += text_moving.speed * time.delta_seconds();
@@ -52,5 +74,6 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_startup_system(startup)
         .add_system(text_system)
+        .add_system(keyboard_spawn_text)
         .run();
 }
